1df7ba56b1d050a79298483c037130a199ea5a0e
Anton Volvenko <volvenko.anthony@gmail.com>
2023-05-05 21:14:17 +0300

    Intermediate push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './css/styles.css';
-
+/* import './css/styles.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+import { fetchCountries } from './fetchCountries';
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const allVars = {
+  countryList: document.querySelector('.country-list'),
+  searchBox: document.querySelector('#search-box'),
+  countryInfo: document.querySelector('.counry-info'),
+}; */
